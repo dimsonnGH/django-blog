67b8444196f4ed85063dda604f442a9bae98787a
dimsonnGH <dimsonn@mail.ru>
2022-11-29 18:35:18 +0400

    Move static files to static folder.

--- sensive_blog/settings.py
+++ sensive_blog/settings.py
@@ -94,7 +94,7 @@ USE_TZ = True
 
 STATIC_URL = os.getenv('STATIC_URL', '/static/')
 
-STATICFILES_DIRS = (BASE_DIR, )
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
 MEDIA_ROOT = os.getenv("MEDIA_ROOT", os.path.join(BASE_DIR, 'media'))
 
